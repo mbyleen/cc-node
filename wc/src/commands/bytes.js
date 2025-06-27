c7e957d33a68bb2148c82dedac22239a2dd7a2db
Marie Byleen <mariebyleen@pop-os.localdomain>
2025-06-27 14:45:20 -0500

    use fs stats for byte count

--- wc/src/commands/bytes.js
+++ wc/src/commands/bytes.js
@@ -3,18 +3,11 @@ import path from "path";
 
 export const countBytes = async (file) => {
   const filePath = path.resolve(process.cwd(), file);
-  const readStream = fs.createReadStream(filePath, { encoding: "utf8" });
 
-  let bytes = 0;
-
-  try {
-    for await (const chunk of readStream) {
-      bytes += chunk.length;
+  fs.stat(filePath, (err, stats) => {
+    if (err) {
+      console.error(`Error reading file: ${error.message}`);
     }
-  } catch (error) {
-    console.error(`Error reading file: ${error.message}`);
-    return null;
-  }
-
-  console.log(bytes);
+    console.log(`${stats.size} ${file}`);
+  });
 };
