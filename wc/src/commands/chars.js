68a645a0042019681055a03a2d7ada7ffa85f8f1
Marie Byleen <mariebyleen@pop-os.localdomain>
2025-08-12 15:24:00 -0500

    count graphemes with -m flag

--- wc/src/commands/chars.js
+++ wc/src/commands/chars.js
@@ -1,35 +1,23 @@
 import fs from "fs";
 
-// This implementation does not respect the system locale
-const isHighSurrogate = (char) => /[\uD800-\uDBFF]/.test(char);
-const isLowSurrogate = (char) => /[\uDC00-\uDFFF]/.test(char);
-
-const countCodePoints = (str) => {
+const countGraphemesWithLocale = (str, locale) => {
+  const segmenter = new Intl.Segmenter(locale, { granularity: "grapheme" });
   let count = 0;
-
-  for (let i = 0; i < str.length; i++) {
-    const currentChar = str[i];
-
-    if (isHighSurrogate(currentChar)) {
-      const isLastChar = i === str.length - 1;
-      const nextChar = str[i + 1];
-
-      // Check for an unpaired UTF-16 surrogate at the end of the input string
-      if (isLastChar) {
-        count += 1;
-        break;
-      }
-
-      // Skip the second code point of a UTF-16 surrogate pair
-      if (isLowSurrogate(nextChar)) {
-        i += 1;
-        count += 1;
-      }
-    } else {
-      count += 1;
-    }
+  for (const _ of segmenter.segment(str)) {
+    count++;
   }
   return count;
+};
+
+const getSystemLocale = () => {
+  const locale =
+    process.env.LC_ALL || process.env.LC_CTYPE || process.env.LANG || "en-US";
+  return locale.replace("_", "-");
+};
+
+const countWithSystemLocale = (str) => {
+  const locale = getSystemLocale().split(".")[0];
+  return countGraphemesWithLocale(str, locale);
 };
 
 export const countChars = async (filePath) => {
@@ -39,7 +27,7 @@ export const countChars = async (filePath) => {
     let characters = 0;
 
     readStream.on("data", (chunk) => {
-      characters += countCodePoints(chunk.toString());
+      characters += countWithSystemLocale(chunk.toString());
     });
 
     readStream.on("end", () => {
